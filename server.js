82db00bee1b4164468ac1ebec9872160ad2ea800
aminumuhammad000 <aminumuhammad00015@gmail.com>
2026-02-27 19:28:59 +0100

    update server URL to api.asasurealty-commission.com.ng

--- server.js
+++ server.js
@@ -246,4 +246,4 @@ app.post('/api/admin/login', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT} & https://api.asasurealty-commission.com.ng`));
